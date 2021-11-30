76c2dc58a83402d8b4a194c8ce44dbb9a700a20e
LivCodes <oliviareeddev@gmail.com>
2021-11-30 16:50:42 -0500

    playing field set

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Space Battle")
+
+var canvas = document.getElementById("asteroids"); 
+var context = canvas.getContext("2d"); 
+context.strokeStyle = 'black'; // the border line
+context.lineWidth = 15; // width of the border line
+context.fillStyle = 'black'; // fill the square
+context.rect(50, 50, 800, 650); // inner black box
+context.stroke();
+context.fill(); // draw the fill the sq lines
