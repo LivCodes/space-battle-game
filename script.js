d162400d4abd454fa4d156de9e7577c0557c5858
LivCodes <oliviareeddev@gmail.com>
2021-11-30 19:11:27 -0500

    styled title

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-console.log("Space Battle")
+const msg= "Space Battle"
 
-var canvas = document.getElementById("asteroids"); 
+var canvas = document.getElementById("space-battle"); 
 var context = canvas.getContext("2d"); 
-context.strokeStyle = 'black'; // the border line
-context.lineWidth = 15; // width of the border line
+context.strokeStyle = 'lightgrey'; // the border line
 context.fillStyle = 'black'; // fill the square
-context.rect(50, 50, 800, 650); // inner black box
+context.lineWidth = 10; // width of the border line
+context.rect(50, 50, canvas.width - 100, canvas.height - 100); // inner black box
 context.stroke();
 context.fill(); // draw the fill the sq lines
+
+// add text 
+context.font = "45px Sans-Serif";
+context.fillStyle = "blue"; 
+context.strokeStyle = 'green';
+context.strokeText(msg, 60, 100);
+context.fillText(msg, 60, 100);
+
+
+
+
