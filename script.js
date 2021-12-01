b16de59136ca90f7089fb30a6fa624fed5251ec2
LivCodes <oliviareeddev@gmail.com>
2021-11-30 19:25:36 -0500

    character creation

--- script.js
+++ script.js
@@ -16,6 +16,28 @@ context.strokeStyle = 'green';
 context.strokeText(msg, 60, 100);
 context.fillText(msg, 60, 100);
 
+//stick figure
+context.strokeStyle = '#FFFFFF';
+context.lineWidth = 2;
+context.beginPath();
+context.arc(200, 140, 20, 0, Math.PI * 2);
+context.moveTo(200, 160);
+context.lineTo(200, 220);
+context.moveTo(180, 300);
+context.lineTo(185, 260);
+context.lineTo(200, 220);
+context.lineTo(215, 260);
+context.lineTo(220, 300);
+context.moveTo(240, 130);
+context.lineTo(225, 170);
+context.lineTo(200, 170);
+context.lineTo(175, 180);
+context.lineTo(170, 220);
+context.moveTo(220, 300);
+context.lineTo(240,300);
+context.moveTo(180, 300);
+context.lineTo(160, 300);
+context.stroke();
 
 
 
